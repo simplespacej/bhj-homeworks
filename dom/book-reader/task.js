0430ee8b55ba7fb3871500829cca65157cd8e1ab
simplespacej <my-ptogramming@mail.ru>
2024-10-03 12:03:47 +0300

    third commit book-reader

---
+++ dom/book-reader/task.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const fontSizeLinks = document.querySelectorAll('.font-size');
+    const book = document.getElementById('book');
+
+    fontSizeLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            fontSizeLinks.forEach(link => {
+                link.classList.remove('font-size_active');
+            });
+
+            link.classList.add('font-size_active');
+
+            const size = link.dataset.size;
+
+            book.classList.remove('book_fs-small', 'book_fs-big');
+
+            if (size === 'small') {
+                book.classList.add('book_fs-small');
+            } else if (size === 'big') {
+                book.classList.add('book_fs-big');
+            }
+        });
+    });
+});
